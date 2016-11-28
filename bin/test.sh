a6a966d353861dcc340073c249b89adc03a609da
Ilja Orlovs <ilja@wise.fish>
2016-11-28 10:46:15 +0000

    Added testing scripts.

---
+++ bin/test.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -e
+
+BIN_DIR=$(python -c "import os.path as p ; print p.dirname(p.realpath('${BASH_SOURCE[0]}'))")
+PROJECT_ROOT=$(dirname "${BIN_DIR}")
+TEST_DIR="${PROJECT_ROOT}/tests"
+
+cd "${PROJECT_ROOT}"
+export PYTHONPATH="${PROJECT_ROOT}:${PYTHONPATH}"
+
+exec pytest ${TEST_DIR} $*
---
+++ tests/lib/ble/test_bgapi.py
@@ -0,0 +1,6 @@
+"""PyProta bgapi library tests."""
+
+import PyPush.lib.ble.bgapi as TestedModule
+
+def test_init():
+	assert 3 == 4
