41be4b50fc0126af5a96de1e72027e85361b2189
Ilja Orlovs <ilja@wise.fish>
2016-12-07 13:22:39 +0000

    Release script updated

--- bin/mk_release.sh
+++ bin/mk_release.sh
@@ -2,6 +2,7 @@
 
 BIN_DIR=$(python -c "import os.path as p ; print p.dirname(p.realpath('${BASH_SOURCE[0]}'))")
 PROJECT_ROOT=$(dirname "${BIN_DIR}")
+REQ_DIR="${PROJECT_ROOT}/requirements"
 
 # Test & validate
 "${BIN_DIR}/test.sh"
@@ -9,8 +10,8 @@ PROJECT_ROOT=$(dirname "${BIN_DIR}")
 
 git checkout release
 git merge --squash master
-pip freeze "${PROJECT_DIR}/requirements/prod.txt" > "${PROJECT_DIR}/requirements/frozen.txt"
-git add "${PROJECT_DIR}/requirements/frozen.txt"
+pip freeze "${REQ_DIR}/prod.txt" > "${REQ_DIR}/frozen.txt"
+git add "${REQ_DIR}/frozen.txt"
 git commit -m "Frozen requirements updated"
 
 git checkout master
