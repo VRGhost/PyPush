5f853f578bf4481fbd0ea42ad6110dcf5e7a549a
Ilja Orlovs <ilja@wise.fish>
2016-12-10 20:21:12 +0000

    Dockerfile fixes mk1

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,4 @@
 FROM python:2.7
-FROM chekote/bower
 
 ENV PORT=5000
 ENV DEVICE="/dev/tty.usbmodem1"
@@ -9,8 +8,7 @@ COPY ./ /usr/src/PyPush
 
 WORKDIR /usr/src/PyPush
 
-RUN bower install --allow-root
-ONBUILD RUN pip install --no-cache-dir -r requirements.txt
+RUN pip install --no-cache-dir -r requirements.txt
 
 EXPOSE $PORT
-CMD ["/PyPush/bin/serve.sh", "--port", $PORT, "--ble_device", $DEVICE]
+CMD ["./bin/serve.sh", "--port", $PORT, "--ble_device", $DEVICE]
--- bin/docker_build.sh
+++ bin/docker_build.sh
@@ -1,8 +1,17 @@
 #!/bin/sh -x
 
 BIN_DIR=$(python -c "import os.path as p ; print p.dirname(p.realpath('${BASH_SOURCE[0]}'))")
-PROJECT_ROOT=$(dirname "${BIN_DIR}"
-)
+PROJECT_ROOT=$(dirname "${BIN_DIR}")
 cd "${PROJECT_ROOT}"
+
+pushd _tmp
+rm -rf *
+git checkout .
+popd
+
+bower install
 docker build -t vrghost/pypush:latest .
 docker push vrghost/pypush
+
+# Run with
+#  docker run --device=/dev/ttyACM0 -e DEVICE='/dev/ttyACM0' -e PORT='59730' -p '59730:59730' --name pypush vrghost/pypush
